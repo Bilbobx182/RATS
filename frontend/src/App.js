74c613f2df7c05911f28ca60738cd510d1254c19
Ciaran O Nuallain <onuallainc@gmail.com>
2020-09-05 19:16:42 +0100

    Getting started with Elastic UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { useQuery } from "@apollo/react-hooks"
 import gql from "graphql-tag"
+import "@elastic/eui/dist/eui_theme_light.css";
+import { EuiButton, EuiFlexGroup, EuiFlexItem } from "@elastic/eui";
+import { EuiFieldSearch, EuiSwitch } from '@elastic/eui';
 
-const Header = () =>
-     (
-        <header lassName='header_banner'>
+const CiaranNavbar = ()  => {
+    return (
+      <EuiFlexGroup gutterSize="s" alignItems="stretch">
+      <EuiFlexItem grow={false}>
+        <EuiButton onClick={() => window.alert("Button clicked")}>
+          Primary
+        </EuiButton>
+      </EuiFlexItem>
+    
+      <EuiFlexItem grow={false}>
+        <EuiButton fill onClick={() => window.alert("Button clicked")}>
+          Filled
+        </EuiButton>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+    )
+}
+
+
+const Header = () => {
+     return (
+        <header className='header_banner'>
             <h1>Rats Job finder</h1>
+            <CiaranNavbar></CiaranNavbar>
         </header>
     );
-
+    };
+  
 
 const GET_USERS = gql`
 {
@@ -24,11 +47,34 @@ const GET_USERS = gql`
 }
 `
 
+const Search = () => {
+  const [isClearable, setIsClearable] = React.useState(true);
+  const [value, setValue] = React.useState('');
+
+  const onChange = e => {
+    setValue(e.target.value);
+  };
+
+  return (
+    /* DisplayToggles wrapper for Docs only */
+
+      <EuiFieldSearch
+        placeholder="Search this"
+        value={value}
+        onChange={e => onChange(e)}
+        isClearable={isClearable}
+        aria-label="Use aria labels when no actual label is in use"
+      />
+  );
+};
+
+
 const Location = ({item : {city}}) => (
   <div className="Location">
       <h1 className="Location--name">{city}</h1>
   </div>
 )
+
 
 
 function App() {
@@ -39,7 +85,8 @@ function App() {
 
   return (
     <main className="App">
-      <Header></Header>
+     <Header></Header>
+     <Search></Search>
       <h1>Items</h1>
       {data.allLocations.edges.map(
         item => (

6926e83d17cb0719d4ab894b016742b705085825
Ciaran O Nuallain <onuallainc@gmail.com>
2020-09-07 20:05:27 +0100

    Implement carding

--- frontend/src/components/component_donutchart.js
+++ frontend/src/components/component_donutchart.js
@@ -1,7 +1,8 @@
 import "@elastic/eui/dist/eui_theme_light.css";
 import React from 'react';
 import {Doughnut} from 'react-chartjs-2';
-
+import { EuiFlexItem, EuiFlexGroup} from "@elastic/eui";
+import {EuiCard} from '@elastic/eui';
 export class CardedDonutChart extends React.Component {
 
     constructor(props) {
@@ -10,8 +11,57 @@ export class CardedDonutChart extends React.Component {
 
     render() {
         return (
-          <div className="cardedDonutChart">
-                <Doughnut data={this.props.graphData} />
+        <div className="cardedDonutChart">
+        <EuiFlexGroup justifyContent="flexEnd">
+        <EuiFlexItem>
+            <EuiCard
+        
+          textAlign="left"
+          title="Card 1"
+          description={
+            <span>
+                    <div className="ChartStyle">
+        <Doughnut  data={this.props.graphData} />
+        </div>
+            </span>  
+            }/>
+
+          </EuiFlexItem>
+
+          <EuiFlexItem>
+            <EuiCard
+        
+          textAlign="left"
+          title="Card 2"
+          description={
+            <span>
+                    <div className="ChartStyle">
+        <Doughnut  data={this.props.graphData} />
+        </div>
+            </span>  
+            }/>
+
+          </EuiFlexItem>
+
+
+
+          <EuiFlexItem>
+            <EuiCard
+        
+          textAlign="left"
+          title="Card 3"
+          description={
+            <span>
+                    <div className="ChartStyle">
+        <Doughnut  data={this.props.graphData} />
+        </div>
+            </span>  
+            }/>
+
+          </EuiFlexItem>
+
+        </EuiFlexGroup>
+        
           </div>
         )
     }
